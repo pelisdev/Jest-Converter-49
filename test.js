85d7344aeb4258817b72e7b011789cb967561780
pelisdev <curielyseguro@gmail.com>
2023-10-30 17:58:59 +0100

    listov2

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ test("One euro should be 1.07 dollars", function() {
     // This is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
-test('Convierte dólares a yenes correctamente', () => {
+test('convertir dolares a yenes', () => {
     const { fromDollarToYen } = require('./app.js');
     const yenes = fromDollarToYen(3.5);
   
@@ -19,7 +19,7 @@ test('Convierte dólares a yenes correctamente', () => {
   
     expect(yenes).toBe(expected);
   });
-  test('Convierte yenes a libras esterlinas correctamente', () => {
+  test('convertir yenes a libras', () => {
     const { fromYanToPound } = require('./app.js');
 
     const pounds = fromYanToPound(1);
